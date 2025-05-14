625d2875875ad429982205081a4e110f8b3d5f81
sy1132 <2224802010369@student.tdmu.edu.vn>
2025-05-14 14:46:51 +0700

    21

--- QLKhoHang/obj/Debug/net9.0/QLKhoHang.AssemblyInfo.cs
+++ QLKhoHang/obj/Debug/net9.0/QLKhoHang.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLKhoHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5261b2cadbb56a11442f577214b19aae56b44afd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e1b29c2a994cc4f8be935eda6fead8480614bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLKhoHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLKhoHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
