b5e1b29c2a994cc4f8be935eda6fead8480614bb
sy1132 <2224802010369@student.tdmu.edu.vn>
2025-05-14 14:34:42 +0700

    33

--- QLKhoHang/Dbcontext.cs
+++ QLKhoHang/Dbcontext.cs
@@ -12,7 +12,7 @@ namespace QLKhoHang.Data
         public DbSet <Category> Categories { get; set; }
         public DbSet <Import> Imports { get; set; }
         public DbSet <Products> Products { get; set; }
-        public DbSet <Supplier> Suppliers { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
         public DbSet <Warehouse> Warehouse { get; set; }
     }
 }
